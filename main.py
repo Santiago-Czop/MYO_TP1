0ed52b1b57c2d698b3296e5981f11a22e2b914df
Santiago-Czop <czopsantiago@gmail.com>
2021-09-19 22:07:23 -0300

    [ADD] - Added program skeleton

---
+++ main.py
@@ -0,0 +1,16 @@
+def main():
+    incompatibility_graph = load_incompatibility_graph()
+    washing_sets = find_washing_sets(incompatibility_graph)
+    generate_output(washing_sets)
+
+    
+def load_incompatibility_graph():
+    pass
+
+def find_washing_sets(inc_graph):
+    pass
+
+def generate_output(washing_sets):
+    pass
+
+main()
