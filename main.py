96322cf61493745278afe7b7dfb6d279acaa4611
Santiago-Czop <czopsantiago@gmail.com>
2021-09-19 22:41:12 -0300

    [UPDATE] - Updated function to load the file

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 def main():
-    incompatibility_graph = get_incompatibility_graph()
+    incompatibility_graph, times_dict = load_incompatibility_graph_and_times_dict()
     washing_sets = find_washing_sets(incompatibility_graph)
     generate_output(washing_sets)
 
     
-def load_incompatibility_graph():
+def load_incompatibility_graph_and_times_dict():
     incompatibility_graph = {}
+    times_dict = {}
     with open("primer_problema.txt", "r") as my_file:
         for line in my_file:
             line = line.rstrip('\n').strip(' ')
-            if line[0] == 'c': continue
-            if line[0] == 'p': continue #No necesito esta info, la obtengo despues
-            if line[0] == 'e': pass # TODO Codear esto
-            if line[0] == 'n': pass # TODO Codear esto
-
+            if line[0] == 'c': continue # A comment
+            if line[0] == 'p': continue # Unnecessary info
+            if line[0] == 'e': load_edge(incompatibility_graph, line)
+            if line[0] == 'n': load_time(times_dict, line)
+    return incompatibility_graph, times_dict
 
 def find_washing_sets(inc_graph):
     pass
@@ -28,5 +29,16 @@ def add_vertex(graph, v):
 #Connects v to w in the graph
 def add_edge(graph, v, w):
     graph[v].add(w)
+
+def load_edge(graph, line):
+    _, cloth_1, cloth_2 = line.split(' ')
+    add_vertex(graph, cloth_1)
+    add_vertex(graph, cloth_2)
+    add_edge(graph, cloth_1, cloth_2)
+    add_edge(graph, cloth_2, cloth_1)
+
+def load_time(times, line):
+    _, cloth, time = line.split(' ')
+    times[cloth] = int(time)
 
 main()
