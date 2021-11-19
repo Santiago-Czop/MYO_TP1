8eff6728f77786a06f58d3f86b4d4595586d57cf
Santiago-Czop <czopsantiago@gmail.com>
2021-11-19 15:41:54 -0300

    [ADD] - Fixed bug in name

--- main_greedy_v2.py
+++ main_greedy_v2.py
@@ -9,7 +9,7 @@ def main():
     incompatibility_graph, times_dict = utils.load_incompatibility_graph_and_times_dict()
     top_result = utils.evaluate_result(times_dict, "best_time.txt")
     washing_sets = find_washing_sets(incompatibility_graph)
-    generate_output(washing_sets)
+    utils.generate_output_coloring(washing_sets)
     result = utils.evaluate_result(times_dict)
     if result < top_result:
         utils.keep_best_result()
@@ -59,7 +59,7 @@ def first_available(used_colors):
             return count
         count += 1
 
-def generate_output(washing_sets):
+def generate_output_coloring(washing_sets):
     with open("output.txt", "w") as output_file:
         for k,v in washing_sets.items():
             output_file.write(f"{k} {v+1}\n")
